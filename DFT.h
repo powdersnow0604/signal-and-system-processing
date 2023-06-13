9fb360266f47472f1d011c6a7c27419578083c71
powdersnow0604 <gundam6324@naver.com>
2023-06-14 08:10:59 +0900

    DFT header

---
+++ DFT.h
@@ -0,0 +1,19 @@
+#ifndef  __DFT_H__
+#define  __DFT_H__
+
+#include "complex_num.h"
+#include "factorization.h"
+#include "plot.h"
+
+void DFT_1d(complex_num* X);
+double* calcAmplitude(complex_num* X);
+
+void FFT_radix_2(complex_num* P);
+void FFT_radix_k(complex_num* P);
+void FFT_common_factor(complex_num* P);
+
+void IFFT_radix_2(complex_num* P);
+void IFFT_radix_k(complex_num* P);
+void IFFT_common_factor(complex_num* P);
+
+#endif
