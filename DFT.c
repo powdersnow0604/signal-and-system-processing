e9ba8c42861fc8cfbd05dd25fa39a927092ead5c
powdersnow0604 <gundam6324@naver.com>
2023-06-20 07:40:18 +0900

    header about DFT and FFT

--- DFT.c
+++ DFT.c
@@ -5,13 +5,21 @@
 /*------------------선언부-------------------*/
 
 static void OnShuffle(complex_num* X, int N, int log2N);
+static void OnShuffle_4(complex_num* X, int N);
 static int OnReverseBitOrder(int index, int LOG2N);
 static void relocate(complex_num* X, int N, int radix, complex_num* temp);
-static int LOG2N(int Num);
+static inline int LOG2N(int Num);
 
 static void OnButterfly_radix_2(complex_num* P, int n,int mode);
-static void OnButterfly_radix_k(complex_num* P, int n, int radix, complex_num* temp, int mode);
-static void OnButterfly_common_factor(complex_num* P, int N, complex_num* temp, int mode);
+static void OnButterfly_radix_4(complex_num* P, int n, int mode);
+#pragma region OnButterfly_radix_k
+static void OnButterfly_radix_k(complex_num* P, int n, int radix, int mode);
+static void OnButterfly_radix_k_s(complex_num* P, int n, int radix, complex_num* temp, int mode);
+#pragma endregion
+#pragma region OnButterfly_common_factor
+static void OnButterfly_common_factor(complex_num* P, int N, int mode);
+static void OnButterfly_common_factor_s(complex_num* P, int N, complex_num* temp, int mode);
+#pragma endregion
 
 /*------------------정의부-------------------*/
 
@@ -37,6 +45,26 @@ static void OnShuffle(complex_num* X, int N, int log2N)
 	free(temp);
 }
 
+static void OnShuffle_4(complex_num* X, int N)
+{
+	int i, j;
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
+
+	for (i = N; i > 1; i /= 4)
+	{
+		memcpy(temp, X, N * sizeof(complex_num));
+		for (j = 0; j < N; j += i)
+		{
+			memcpy(X + j,           temp + j,         i / 4 * sizeof(complex_num));
+			memcpy(X + j + i / 4,   temp + j + i/4*2, i / 4 * sizeof(complex_num));
+			memcpy(X + j + i / 4*2, temp + j + i/4,   i / 4 * sizeof(complex_num));
+			memcpy(X + j + i / 4*3, temp + j + i/4*3, i / 4 * sizeof(complex_num));
+
+		}
+	}
+	free(temp);
+}
+
 static int OnReverseBitOrder(int index, int LOG2N)
 {
 	int i, X, Y = 0;
@@ -49,28 +77,20 @@ static int OnReverseBitOrder(int index, int LOG2N)
 	return Y;
 }
 
-static int LOG2N(int Num)
+static inline int LOG2N(int Num)
 {
-	int log2N = 0;
-
-	while (Num >= 2)
-	{
-		Num >>= 1;
-		log2N++;
-	}
-
-	return log2N;
+	return (int)(log(Num) / log(2));
 }
 
 static void relocate(complex_num* X, int N, int radix, complex_num* temp)
 {
-	int i, j, pos;
+	int i, j, pos, N_radix = N / radix;
 	
 	for (i = 0; i < N; i += radix) {
 		pos = i / radix;
 		for (j = 0; j < radix; j++) {
-			temp[pos + N / radix * j].Re = X[i + j].Re;
-			temp[pos + N / radix * j].Im = X[i + j].Im;
+			temp[pos + N_radix * j].Re = X[i + j].Re;
+			temp[pos + N_radix * j].Im = X[i + j].Im;
 		}
 	}
 
@@ -122,21 +142,75 @@ double* calcAmplitude(complex_num* X)
 	return temp;
 }
 
+#pragma region FFT_radix_2
 void FFT_radix_2(complex_num* P)
 {
-	int N,log2N;
+	int N;
 
 	N = (int)(_msize(P) / sizeof(complex_num));
 
-	log2N = LOG2N(N);
+	OnShuffle(P, N, LOG2N(N));
 
-	OnShuffle(P, N, log2N);
+	if (errno == ENOMEM) return;
 
-	if (errno = ENOMEM) return;
-
-	OnButterfly_radix_2(P, N,0);
+	OnButterfly_radix_2(P, N, 0);
 }
 
+void IFFT_radix_2(complex_num* P)
+{
+	int i, N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	OnShuffle(P, N, LOG2N(N));
+
+	if (errno == ENOMEM) return;
+
+	OnButterfly_radix_2(P, N, 1);
+
+	for (i = 0; i < N; i++) {
+		P[i].Re /= N;
+		P[i].Im /= N;
+	}
+}
+#pragma endregion
+
+#pragma region FFT_radix_4
+void FFT_radix_4(complex_num* P)
+{
+	int N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	OnShuffle(P, N, LOG2N(N));
+	OnShuffle_4(P, N);
+	
+	if (errno == ENOMEM) return;
+
+	OnButterfly_radix_4(P, N, 0);
+}
+
+void IFFT_radix_4(complex_num* P)
+{
+	int i,N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	OnShuffle(P, N, LOG2N(N));
+	OnShuffle_4(P, N);
+
+	if (errno == ENOMEM) return;
+
+	OnButterfly_radix_4(P, N, 1);
+
+	for (i = 0; i < N; i++) {
+		P[i].Re /= N;
+		P[i].Im /= N;
+	}
+}
+#pragma endregion
+
+#pragma region FFT_radix_k
 void FFT_radix_k(complex_num* P)
 {
 	int N, radix;
@@ -145,68 +219,55 @@ void FFT_radix_k(complex_num* P)
 
 	radix = minFactor(N);
 
-	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * (N + radix - 1));
-
-	if (temp == NULL) return;
-
-	OnButterfly_radix_k(P, N, radix, temp, 0);
-
-	free(temp);
+	OnButterfly_radix_k(P, N, radix, 0);
 }
 
-void FFT_common_factor(complex_num* P)
+void FFT_radix_k_s(complex_num* P)
 {
-	int N;
-	int cnt;
-
-	N = (int)(_msize(P) / sizeof(complex_num));
-
-	if (N == minFactor(N)) cnt = 2 * N;
-	else cnt = N+(int)sqrt(N);
-
-#ifdef SPEED
-	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * cnt);
-
-	if (temp == NULL) return;
-
-	OnButterfly_common_factor(P, N, temp,0);
-
-	free(temp);
-#else
-	OnButterfly_common_factor(P, N, NULL, 0);
-#endif
-}
-
-void IFFT_radix_2(complex_num* P)
-{
-	int i,N,log2N;
-
-	N = (int)(_msize(P) / sizeof(complex_num));
-
-	log2N = LOG2N(N);
-
-	OnShuffle(P, N, log2N);
-	OnButterfly_radix_2(P, N, 1);
-
-	for (i = 0; i < N; i++) {
-		P[i].Re /= N;
-		P[i].Im /= N;
-	}
-}
-
-void IFFT_radix_k(complex_num* P)
-{
-	int i,N,radix;
+	int N, radix;
 
 	N = (int)(_msize(P) / sizeof(complex_num));
 
 	radix = minFactor(N);
 
-	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * (N + radix - 1));
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
 
 	if (temp == NULL) return;
 
-	OnButterfly_radix_k(P, N, radix, temp, 1);
+	OnButterfly_radix_k_s(P, N, radix, temp, 0);
+
+	free(temp);
+}
+
+void IFFT_radix_k(complex_num* P)
+{
+	int i, N, radix;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	radix = minFactor(N);
+
+	OnButterfly_radix_k(P, N, radix, 1);
+
+	for (i = 0; i < N; i++) {
+		P[i].Re /= N;
+		P[i].Im /= N;
+	}
+}
+
+void IFFT_radix_k_s(complex_num* P)
+{
+	int i, N, radix;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	radix = minFactor(N);
+
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
+
+	if (temp == NULL) return;
+
+	OnButterfly_radix_k_s(P, N, radix, temp, 1);
 
 	free(temp);
 
@@ -214,34 +275,68 @@ void IFFT_radix_k(complex_num* P)
 		P[i].Re /= N;
 		P[i].Im /= N;
 	}
+}
+#pragma endregion
+
+#pragma region FFT_common_factor
+void FFT_common_factor(complex_num* P)
+{
+	int N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	OnButterfly_common_factor(P, N, 0);
+}
+
+void FFT_common_factor_s(complex_num* P)
+{
+	int N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
+
+	if (temp == NULL) return;
+
+	OnButterfly_common_factor_s(P, N, temp, 0);
+
+	free(temp);
 }
 
 void IFFT_common_factor(complex_num* P)
 {
 	int i,N;
-	int cnt;
 
 	N = (int)(_msize(P) / sizeof(complex_num));
 
-	if (N == minFactor(N)) cnt = 2 * N;
-	else cnt = N + (int)sqrt(N);;
-#ifdef SPEED
-	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * cnt);
-
-	if (temp == NULL) return;
-
-	OnButterfly_common_factor(P, N, temp, 1);
-
-	free(temp);
-#else
-	OnButterfly_common_factor(P, N, NULL, 1);
-#endif
+	OnButterfly_common_factor(P, N, 1);
 
 	for (i = 0; i < N; i++) {
 		P[i].Re /= N;
 		P[i].Im /= N;
 	}
 }
+
+void IFFT_common_factor_s(complex_num* P)
+{
+	int i, N;
+
+	N = (int)(_msize(P) / sizeof(complex_num));
+
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
+
+	if (temp == NULL) return;
+
+	OnButterfly_common_factor_s(P, N, temp, 1);
+
+	free(temp);
+
+	for (i = 0; i < N; i++) {
+		P[i].Re /= N;
+		P[i].Im /= N;
+	}
+}
+#pragma endregion
 ///////////////////////////////
 
 
@@ -265,35 +360,54 @@ static void OnButterfly_radix_2(complex_num* P, int n, int mode)
 	}
 }
 
-static void OnButterfly_radix_k(complex_num* P, int n, int radix, complex_num* temp, int mode)
+static void OnButterfly_radix_4(complex_num* P, int n, int mode)
+{
+	if (n == 1)
+		return;
+	int i, s = 1 - 2 * mode;
+	complex_num temp1, temp2, temp3;
+
+	for (i = 0; i < 4; i++)
+	{
+		OnButterfly_radix_4(P + n / 4 * i, n / 4 , mode);
+	}
+
+	for (i = 0; i < n / 4; i++)
+	{
+		temp2 = complexMul(twiddle_factor(n, s * ((2 * i) % n)), (P + n / 4 * 2)[i]);
+		temp1 = complexMul(twiddle_factor(n, s * (i % n)), (P + n / 4)[i]);
+		temp3 = complexMul(twiddle_factor(n, s * ((3 * i) % n)), (P + n / 4 * 3)[i]);
+
+		P[i + n / 4 * 3] = complexAdd(complexSub(P[i], temp2), complexMul(I, complexSub(temp1, temp3)));
+		P[i + n / 4 * 1] = complexAdd(complexSub(P[i], temp2), complexMul(I, complexSub(temp3, temp1)));
+		P[i + n / 4 * 2] = complexSub(complexAdd(P[i], temp2), complexAdd(temp1, temp3));
+		P[i] = complexAdd(complexAdd(P[i], temp2), complexAdd(temp1, temp3));
+	}
+}
+
+#pragma region OnButterfly_radix_k
+static void OnButterfly_radix_k(complex_num* P, int n, int radix, int mode)
 {
 	//setting
 	if (n == 1)
 		return;
 
-	int i,j,k;
+	int i,j,k,s=1-2*mode;
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * n);
 
 	//relocate
 	if(n != radix)
 		relocate(P, n, radix,temp);
-	
+	free(temp);
+
 	//decompose
 	for (i = 0; i < radix; i++) {
-		OnButterfly_radix_k(P + i * n / radix, n / radix, radix,temp,mode);
+		OnButterfly_radix_k(P + i * n / radix, n / radix, radix,mode);
 	}
+
+	temp = (complex_num*)malloc(sizeof(complex_num) * (n+radix-1));
 
 	//conquer
-	if (mode) {
-		for (i = 0; i < n; i++) {
-			temp[i + radix - 1] = twiddle_factor(n, -i);
-		}
-	}
-	else {
-		for (i = 0; i < n; i++) {
-			temp[i + radix - 1] = twiddle_factor(n, i);
-		}
-	}
-
 	for (i = 0; i < n / radix; i++)
 	{
 		for (j = 0; j < radix-1; j++) {
@@ -304,50 +418,73 @@ static void OnButterfly_radix_k(complex_num* P, int n, int radix, complex_num* t
 		{
 			P[i + n / radix * j] = P[i];
 			for (k = 1; k < radix; k++) {
-				P[i + n / radix * j] = complexAdd(P[i + n / radix * j], complexMul(temp[k-1], temp[radix-1+((i + n / radix * j) * k) % n]));
+				P[i + n / radix * j] = complexAdd(P[i + n / radix * j], complexMul(temp[k-1], twiddle_factor(n, s * ((i + n / radix * j) * k) % n)));
 			}
 		}
 	}
-
+	free(temp);
 }
 
-static void OnButterfly_common_factor(complex_num* P, int N, complex_num* temp, int mode)
+static void OnButterfly_radix_k_s(complex_num* P, int n, int radix, complex_num* temp, int mode)
+{
+	//setting
+	if (n == 1)
+		return;
+
+	int i, j, k, s=1-2*mode;
+
+	//relocate
+	if (n != radix)
+		relocate(P, n, radix, temp);
+
+	//decompose
+	for (i = 0; i < radix; i++) {
+		OnButterfly_radix_k_s(P + i * n / radix, n / radix, radix, temp, mode);
+	}
+
+	//conquer
+	for (i = 0; i < n / radix; i++)
+	{
+		for (j = 0; j < radix - 1; j++) {
+			temp[j] = P[i + n / radix * (j + 1)];
+		}
+
+		for (j = radix - 1; j >= 0; j--)
+		{
+			P[i + n / radix * j] = P[i];
+			for (k = 1; k < radix; k++) {
+				P[i + n / radix * j] = complexAdd(P[i + n / radix * j], complexMul(temp[k - 1], twiddle_factor(n, s * ((i + n / radix * j) * k) % n)));
+			}
+		}
+	}
+}
+#pragma endregion
+
+#pragma region OnButterfly_common_factor
+static void OnButterfly_common_factor(complex_num* P, int N, int mode)
 {
 	//setting
 	if (N == 1)
 		return;
 
-	int i,j,k,radix = minFactor(N);
+	int i,j,k,radix = minFactor(N), s = 1 - 2*mode;
 
-	#ifndef SPEED
-	temp = (complex_num*)malloc(sizeof(complex_num) * (N + radix));
-	#endif
+	complex_num* temp = (complex_num*)malloc(sizeof(complex_num) * N);
 
-	relocate(P, N, radix, temp);
+	if(N != radix)
+		relocate(P, N, radix, temp);
+
+	free(temp);
 
 	//decompose
 	for (i = 0; i < radix; i++)
 	{
-	#ifdef SPEED
-		OnButterfly_common_factor(P + N / radix * i, N / radix, temp,mode);
-	#else
-		OnButterfly_common_factor(P + N / radix * i, N / radix, NULL, mode);
-	#endif
-
+		OnButterfly_common_factor(P + N / radix * i, N / radix, mode);
 	}
+
+	temp = (complex_num*)malloc(sizeof(complex_num) * (N + radix - 1));
 
 	//conquer
-	if (mode) {
-		for (i = 0; i < N; i++) {
-			temp[i + radix - 1] = twiddle_factor(N, -i);
-		}
-	}
-	else {
-		for (i = 0; i < N; i++) {
-			temp[i + radix - 1] = twiddle_factor(N, i);
-		}
-	}
-
 	for (i = 0; i < N / radix; i++)
 	{
 		for (j = 0; j < radix-1; j++) {
@@ -359,12 +496,46 @@ static void OnButterfly_common_factor(complex_num* P, int N, complex_num* temp, 
 			
 			P[i + N / radix * j] = P[i];
 			for (k = 1; k < radix; k++) {
-				P[i + N / radix * j] = complexAdd(P[i + N / radix * j], complexMul(temp[k - 1], temp[radix-1 + ((i + N / radix * j) * k) % N]));
+				P[i + N / radix * j] = complexAdd(P[i + N / radix * j], complexMul(temp[k - 1], twiddle_factor(N, s*((i + N / radix * j) * k) % N)));
 			}
 		}
 	}
-	#ifndef SPEED
-		free(temp); 
-	#endif
+	free(temp); 
 }
+
+static void OnButterfly_common_factor_s(complex_num* P, int N, complex_num* temp, int mode)
+{
+	//setting
+	if (N == 1)
+		return;
+
+	int i, j, k, radix = minFactor(N), s = 1 - 2*mode;
+
+	if (N != radix)
+		relocate(P, N, radix, temp);
+
+	//decompose
+	for (i = 0; i < radix; i++)
+	{
+		OnButterfly_common_factor_s(P + N / radix * i, N / radix, temp, mode);
+	}
+
+	//conquer
+	for (i = 0; i < N / radix; i++)
+	{
+		for (j = 0; j < radix - 1; j++) {
+			temp[j] = P[i + N / radix * (j + 1)];
+		}
+
+		for (j = radix - 1; j >= 0; j--)
+		{
+
+			P[i + N / radix * j] = P[i];
+			for (k = 1; k < radix; k++) {
+				P[i + N / radix * j] = complexAdd(P[i + N / radix * j], complexMul(temp[k - 1],twiddle_factor(N,s*((i + N / radix * j) * k) % N)));
+			}
+		}
+	}
+}
+#pragma endregion
 ///////////////////////////////////
--- DFT.h
+++ DFT.h
@@ -1,8 +1,6 @@
 #ifndef  __DFT_H__
 #define  __DFT_H__
 
-#undef SPEED
-
 #include "complex_num.h"
 #include "factorization.h"
 #include "plot.h"
@@ -11,11 +9,22 @@ void DFT_1d(complex_num* X);
 double* calcAmplitude(complex_num* X);
 
 void FFT_radix_2(complex_num* P);
+void IFFT_radix_2(complex_num* P);
+
+void FFT_radix_4(complex_num* P);
+void IFFT_radix_4(complex_num* P);
+
+#ifdef SPEED
+void FFT_radix_k_s(complex_num* P);
+void FFT_common_factor_s(complex_num* P);
+
+void IFFT_radix_k_s(complex_num* P);
+void IFFT_common_factor_s(complex_num* P);
+#else
 void FFT_radix_k(complex_num* P);
 void FFT_common_factor(complex_num* P);
 
-void IFFT_radix_2(complex_num* P);
 void IFFT_radix_k(complex_num* P);
 void IFFT_common_factor(complex_num* P);
-
+#endif
 #endif
